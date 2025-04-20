e0a5863b5f4dde85c7b6efa53ba303fa54594c8b
Austin Robertson <robertsonaustin27@gmail.com>
2025-04-20 14:50:24 -0400

    created header fade in on page load

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import "./styles.css";
 import Todo from "./Todo.js";
 import TodoList from "./TodoList.js";
 
-const todo1 = new Todo("Task 1", "My first task", "March 9", 3);
+import { headerStartup } from "./domFunctions.js";
 
-todo1.completed = true;
-
-console.log(todo1.completed);
+headerStartup();
